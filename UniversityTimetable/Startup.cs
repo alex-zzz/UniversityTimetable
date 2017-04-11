6ae69e0903d7cb45cc67b0554d93b2b8bd864a3e
Alex <zzzqqq@outlook.com>
2017-04-11 13:26:25 +0300

    Add project files.

---
+++ UniversityTimetable/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UniversityTimetable.Startup))]
+namespace UniversityTimetable
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
