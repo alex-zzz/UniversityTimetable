623c36f30559b5b18bec8424f9996d7b4cbf003c
Alex <zzzqqq@outlook.com>
2017-04-12 17:55:40 +0300

    FullName

---
+++ UniversityTimetable.DAL/Identity/IdentityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityTimetable.DAL.Identity
+{
+    public static class IdentityExtensions
+    {
+        public static string GetUserName(this IIdentity identity) {
+            return identity
+        }
+    }
+}
