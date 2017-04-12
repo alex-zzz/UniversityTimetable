6672ac6a1cbc64f17192eced516153af43794817
Alex <zzzqqq@outlook.com>
2017-04-12 18:41:09 +0300

    AdminPage

---
+++ UniversityTimetable/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UniversityTimetable.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        [Authorize (Roles = "admin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
